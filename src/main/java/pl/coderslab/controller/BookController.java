28f675a2b43f1466a0785ba740bedb74c6610e38
pawelkozolub <pkozolub@gmail.com>
2023-09-23 21:29:47 +0200

    add logger to BookController + Book class update > toString()

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -1,11 +1,13 @@
 package pl.coderslab.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import pl.coderslab.model.Book;
 import pl.coderslab.service.BookService;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/books")
 public class BookController {
@@ -20,26 +22,31 @@ public class BookController {
 
     @GetMapping
     public List<Book> getList() {
+        log.info("Get all books");
         return bookService.getBooks();
     }
 
     @GetMapping("/{id}")
     public Book getBook(@PathVariable(name = "id") Long id) {
+        log.info("Get book with id: {}", id);
         return bookService.get(id).orElse(null);
     }
 
     @PostMapping
     public void addBook(@RequestBody Book book) {
+        log.info("Add book: {}", book.toString());
         bookService.add(book);
     }
 
     @DeleteMapping("/{id}")
     public void deleteBook(@PathVariable(name = "id") Long id) {
+        log.info("Delete book with id: {}", id);
         bookService.delete(id);
     }
 
     @PutMapping
     public void updateBook(@RequestBody Book book) {
+        log.info("Update book: {}", book.toString());
         bookService.update(book);
     }
 
--- src/main/java/pl/coderslab/model/Book.java
+++ src/main/java/pl/coderslab/model/Book.java
@@ -1,12 +1,10 @@
 package pl.coderslab.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class Book {
