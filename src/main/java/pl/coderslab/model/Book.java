1a60621475191c590fb180bfe5c16e95dbacc45f
pawelkozolub <pkozolub@gmail.com>
2023-09-23 15:47:08 +0200

    add Book class

---
+++ src/main/java/pl/coderslab/model/Book.java
@@ -0,0 +1,18 @@
+package pl.coderslab.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    private Long id;
+    private String isbn;
+    private String author;
+    private String publisher;
+    private String type;
+}
