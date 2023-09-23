8f445928c4778da0f20e26331e74d6037c59c4e4
pawelkozolub <pkozolub@gmail.com>
2023-09-23 21:07:54 +0200

    add configureDefaultServletHandling overriding to AppConfig + change in index.jsp to account for static resource (css)

--- src/main/java/pl/coderslab/configuration/AppConfig.java
+++ src/main/java/pl/coderslab/configuration/AppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -19,5 +20,12 @@ public class AppConfig implements WebMvcConfigurer {
         viewResolver.setPrefix("WEB-INF/views/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
+    }
+
+    // to handle static resources (css, pictures, etc.) need to override default method
+    // otherwise Spring will look for controller methods to deal with them which do not exist
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
     }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,10 +1,12 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page isELIgnored="false" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>Index</title>
-<%--    <link rel="stylesheet" type="text/css" href="/css/simple.css">--%>
-    <style><%@include file="/css/simple.css"%></style>
+    <link rel="stylesheet" type="text/css" href="<c:url value="/css/simple.css"/>">
+    <%-- alternative that not require configureDefaultServletHandling overriding --%>
+    <%--    <style><%@include file="/css/simple.css"%></style>--%>
 </head>
 <body>
     <h1>Web Application Index Page</h1>
