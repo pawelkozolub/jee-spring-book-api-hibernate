4e872da9724c69188227ddbddfa2fa994977fefe
pawelkozolub <pkozolub@gmail.com>
2023-09-23 16:11:58 +0200

    add Book class

--- src/main/java/pl/coderslab/model/Book.java
+++ src/main/java/pl/coderslab/model/Book.java
@@ -12,6 +12,7 @@ import lombok.Setter;
 public class Book {
     private Long id;
     private String isbn;
+    private String title;
     private String author;
     private String publisher;
     private String type;
