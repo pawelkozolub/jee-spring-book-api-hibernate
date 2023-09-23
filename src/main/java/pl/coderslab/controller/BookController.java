7b7b1881c56f26e0bb58554a8243c4ef6ba4d084
pawelkozolub <pkozolub@gmail.com>
2023-09-23 17:34:06 +0200

    add BookController with full functionalities as defined in BookService interphase

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -1,13 +1,49 @@
 package pl.coderslab.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.coderslab.model.Book;
+import pl.coderslab.service.BookService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
+    private BookService bookService;
+
+    // injection via class constructor > note also that we use interphase here, not a specific implementation
+    // such as MockBookService > in future it allows us to change MockBookService into other e.g. FileBookService
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> getList() {
+        return bookService.getBooks();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBook(@PathVariable(name = "id") Long id) {
+        return bookService.get(id).orElse(null);
+    }
+
+    @PostMapping
+    public void addBook(@RequestBody Book book) {
+        bookService.add(book);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable(name = "id") Long id) {
+        bookService.delete(id);
+    }
+
+    @PutMapping
+    public void updateBook(@RequestBody Book book) {
+        bookService.update(book);
+    }
+
+    // helloBook() is used for testing
     @RequestMapping("/helloBook")
     public Book helloBook() {
         return new Book(
