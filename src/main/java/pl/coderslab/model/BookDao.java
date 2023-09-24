607b4c37fd8a6a8e218785d845abbfc6bc41ccc3
pawelkozolub <pkozolub@gmail.com>
2023-09-24 12:22:02 +0200

    BookDao, add methods: update > merge, delete > remove, findById() > find, findAll > createQuery

--- src/main/java/pl/coderslab/model/BookDao.java
+++ src/main/java/pl/coderslab/model/BookDao.java
@@ -5,6 +5,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 @Transactional
@@ -15,5 +16,21 @@ public class BookDao {
 
     public void save(Book entity) {
         entityManager.persist(entity);
+    }
+
+    public void update(Book entity) {
+        entityManager.merge(entity);
+    }
+
+    public void delete(Book entity) {
+        entityManager.remove(entityManager.contains(entity) ? entity : entityManager.merge(entity));
+    }
+
+    public Book findById(Long id) {
+        return entityManager.find(Book.class, id);
+    }
+
+    public List<Book> findAll() {
+        return entityManager.createQuery("SELECT b FROM Book b", Book.class).getResultList();
     }
 }
