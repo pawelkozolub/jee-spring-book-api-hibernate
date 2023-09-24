cc7fdddb6efea147661259228fa0dce1edeb15aa
pawelkozolub <pkozolub@gmail.com>
2023-09-24 20:29:50 +0200

    BookDaoController, set common RequestMapping at class definition

--- src/main/java/pl/coderslab/controller/BookDaoController.java
+++ src/main/java/pl/coderslab/controller/BookDaoController.java
@@ -8,6 +8,7 @@ import pl.coderslab.model.Book;
 import pl.coderslab.model.BookDao;
 
 @Controller
+@RequestMapping("/book")
 public class BookDaoController {
 
     private final BookDao bookDao;
@@ -16,19 +17,19 @@ public class BookDaoController {
         this.bookDao = bookDao;
     }
 
-    @RequestMapping("/book/list")
+    @RequestMapping("/list")
     @ResponseBody
     public String getAll() {
         return bookDao.findAll().toString();
     }
 
-    @RequestMapping("/book/{id}")
+    @RequestMapping("/{id}")
     @ResponseBody
     public String get(@PathVariable(name = "id") Long id) {
         return bookDao.findById(id).toString();
     }
 
-    @RequestMapping("/book/update/{id}/{title}/{author}")
+    @RequestMapping("/update/{id}/{title}/{author}")
     @ResponseBody
     public String updateTitleAuthor(@PathVariable(name = "id") Long id,
                                     @PathVariable(name = "title") String title,
@@ -40,7 +41,7 @@ public class BookDaoController {
         return "Book updated:\n" + book.toString();
     }
 
-    @RequestMapping("/book/delete/{id}")
+    @RequestMapping("/delete/{id}")
     @ResponseBody
     public String delete(@PathVariable(name = "id") Long id) {
         Book book = bookDao.findById(id);
@@ -48,7 +49,7 @@ public class BookDaoController {
         return "Book deleted:\n" + book.toString();
     }
 
-    @RequestMapping("/book/add-sample-book")
+    @RequestMapping("/add-sample-book")
     @ResponseBody
     public String addSampleBook() {
         Book book = new Book();
