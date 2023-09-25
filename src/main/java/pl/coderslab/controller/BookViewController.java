544e1e9907f3aed2f6245634d5128edf8e125d06
pawelkozolub <pkozolub@gmail.com>
2023-09-26 00:22:29 +0200

    BookViewController, add methods: edit, update

--- src/main/java/pl/coderslab/controller/BookViewController.java
+++ src/main/java/pl/coderslab/controller/BookViewController.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.model.Book;
 import pl.coderslab.service.BookService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -44,5 +46,18 @@ public class BookViewController {
         return "redirect:/view/book/list";
     }
 
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable(name = "id") Long id, Model model) {
+        model.addAttribute("book", bookService.get(id));
+        return "/books/edit-view";
+    }
 
+    @PostMapping("/edit")
+    public String update(@Valid Book book, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/books/edit-view";
+        }
+        bookService.update(book);
+        return "redirect:/view/book/list";
+    }
 }
